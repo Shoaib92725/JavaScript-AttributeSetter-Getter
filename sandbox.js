171daf20ed1898d1bff0f94b2e82a7aca82a55c6
Mohammed Shoaib <93986760+Shoaib92725@users.noreply.github.com>
2024-10-24 16:17:49 +0530

    Add files via upload
    Adding and Removing classes using JavaScript

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,16 @@
-const a = document.querySelector(".one");
-console.log(a.innerText);
-//changing the text inside the .one class
-a.innerText = "Hi, I am level 2 heading";
-//printing the changed result
-console.log("Changed text : " + a.innerText);
+//Adding And Removing classes by using classList.add('') & classList.remove('')
+// const  content = document.querySelector('p');
+// console.log(content.classList);
+// content.classList.add('fruit');
+// content.classList.remove('fruit');
+// content.classList.add('vegetable');
 
-//selecting all the h3 tags for replacing with userDefinedValues
-const b = document.querySelectorAll("h3");
-b.forEach((x) => {
-  console.log(x.innerHTML);//to find what is inside the tag
-  x.innerText = "Hi, I am level 3 heading";
-  console.log(x.innerText);
-});
-
-
-//changing the innerHTML using document.querySelector()
-const content = document.querySelector('.content');
-console.log('Before changing',content.innerHTML);//before changing
-content.innerHTML ='<span>  Hi,i am span tag </span>';
-console.log('After changing',content.innerHTML);//after changing
-
-//Adding more tags using document.querySelector()
-//changing the innerHTML using document.querySelector()
-const content1 = document.querySelector('.content');
-const countries = ['India','China','Nepal'];
-countries.forEach(country=>{
-    content1.innerHTML += `<p>${country}</p>`;
+const  content = document.querySelectorAll('p');
+content.forEach(element => {
+    if(element.textContent.includes('fruit')){
+        element.classList.add('fruit');
+    }
+    else if(element.textContent.includes('vegetable')){
+        element.classList.add('vegetable');
+    }
 });
